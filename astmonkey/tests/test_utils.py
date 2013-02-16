e032bdab80c1f74bd095ae651e1f40d1e2bf550c
Konrad Hałas <halas.konrad@gmail.com>
2013-02-16 23:50:41 +0100

    Added few is_docstring tests.

--- astmonkey/tests/test_utils.py
+++ astmonkey/tests/test_utils.py
@@ -14,3 +14,14 @@ class IsDocstringTest(unittest.TestCase):
         node = transformers.ParentNodeTransformer().visit(ast.parse('"""doc"""'))
 
         self.assertTrue(utils.is_docstring(node.body[0].value))
+
+    def test_function_docstring_node(self):
+        node = transformers.ParentNodeTransformer().visit(ast.parse('def foo():\n\t"""doc"""'))
+
+        self.assertTrue(utils.is_docstring(node.body[0].body[0].value))
+
+    def test_class_docstring_node(self):
+        node = transformers.ParentNodeTransformer().visit(ast.parse('class X:\n\t"""doc"""'))
+
+        self.assertTrue(utils.is_docstring(node.body[0].body[0].value))
+
