1b038d8563dd934f24ff539d3c59637e20c5bfe1
Konrad Hałas <halas.konrad@gmail.com>
2013-02-17 15:43:41 +0100

    Added some graph layout stuff.

--- astmonkey/visitors.py
+++ astmonkey/visitors.py
@@ -5,7 +5,7 @@ import pydot
 class GraphNodeVisitor(ast.NodeVisitor):
 
     def __init__(self):
-        self.graph = pydot.Dot(graph_type='graph')
+        self.graph = pydot.Dot(graph_type='graph', **self._dot_graph_kwargs())
 
     def visit(self, node):
         if len(node.parents) <= 1:
@@ -14,8 +14,11 @@ class GraphNodeVisitor(ast.NodeVisitor):
             self.graph.add_edge(self._dot_edge(node))
         super(GraphNodeVisitor, self).visit(node)
 
+    def _dot_graph_kwargs(self):
+        return {}
+
     def _dot_node(self, node):
-        return pydot.Node(str(node), label=self._dot_node_label(node))
+        return pydot.Node(str(node), label=self._dot_node_label(node), **self._dot_node_kwargs(node))
 
     def _dot_node_label(self, node):
         fields_labels = []
@@ -30,12 +33,23 @@ class GraphNodeVisitor(ast.NodeVisitor):
                     fields_labels.append('{0}={1}'.format(field, value_label))
         return 'ast.{0}({1})'.format(node.__class__.__name__, ', '.join(fields_labels))
 
+    def _dot_node_kwargs(self, node):
+        return {
+            'shape': 'box',
+            'fontname': 'Curier'
+        }
+
     def _dot_edge(self, node):
-        return pydot.Edge(str(node.parent), str(node), label=self._dot_edge_label(node))
+        return pydot.Edge(str(node.parent), str(node), label=self._dot_edge_label(node), **self._dot_edge_kwargs(node))
 
     def _dot_edge_label(self, node):
         label = node.parent_field
         if not node.parent_field_index is None:
             label += '[{0}]'.format(node.parent_field_index)
         return label
+
+    def _dot_edge_kwargs(self, node):
+        return {
+            'fontname': 'Curier'
+        }
 
