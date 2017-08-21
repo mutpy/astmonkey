7f807ee47bf9742fd6a1d1ddd89c29455625f6c5
Philipp Hossner <philipp.hossner@posteo.de>
2017-08-21 14:57:59 +0200

    Drop support for Python 3.1 and 3.2

--- setup.py
+++ setup.py
@@ -14,8 +14,6 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
